903b68eb8941cb7de31ce52c314d3a68102d1f1d
Adam Page <pageadj@gmail.com>
2021-01-19 23:44:56 -0800

    delete temp script file and fix the remaining tests

--- temp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-# helm uninstall -n bosn bosn
-make package
-make all DOCKER_REPO=localhost:32000/bosn/
-make push DOCKER_REPO=localhost:32000/bosn/
-helm install bosn -n bosn deploy/boatswain/ -f temp.yaml 
